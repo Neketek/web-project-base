103e37d3c37f8058917950b317d3827ac37b86ca
Neketek <neketek@gmail.com>
2018-01-31 23:02:00 +0200

    templates folder moved to view modules folder. I want to keep related things together. Views folder will contain Forms which will be rendered using templates

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from modules.config import flask_app
 from modules.models.sql.session import Session
-app = Flask(__name__)
+from flask import render_template
+app = Flask(__name__,template_folder=flask_app.TEMPLATE_FOLDER)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    session = Session()
-    session.close()
-    app.run(debug=True,host="0.0.0.0",port=8080,threaded=True)
+    # session = Session()
+    # session.close()
+    app.run(debug=True,host="0.0.0.0",port=8081,threaded=True)
--- server/modules/config/flask_app.py
+++ server/modules/config/flask_app.py
@@ -1,2 +1,4 @@
 class Config:
     MAX_CONTENT_LENGTH = 1024*1024*8
+
+TEMPLATE_FOLDER = "modules/views/templates"
