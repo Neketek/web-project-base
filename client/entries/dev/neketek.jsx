5224b6a07d0aac475955608adfd459f3fef62505
Neketek <neketek@gmail.com>
2018-02-12 18:21:33 +0200

    Dev and production server routing which allows usage of the custom app & landing entries

--- client/entries/dev/neketek.jsx
+++ client/entries/dev/neketek.jsx
@@ -4,8 +4,12 @@ import App from 'modules/app/index'; // app entry component
 
 render(App,"#root"); // initial render
 
+//important part which enables hot reloading
+// for some reason it will not work if this code will be wrapped in function
 if(module.hot){
   // update app using HMR (hot module reloading)
+  // technically here you are informing webpack that you are waiting for updates for 'modules/app/index'
+  // accept function has a callback parameter which should update root component
   module.hot.accept('modules/app/index', () => {
     const NextApp = require('modules/app/index').default;
     render(NextApp,"#root");
--- server/__init__.py
+++ server/__init__.py
@@ -1,44 +1,15 @@
 from flask import Flask
 from modules.config import flask_app
-from flask import render_template
-# from modules.models.sql.session import SQL_DB_ENGINE
-# from modules.constants import database
-# from modules.models.sql import User
-# from modules.routing.utils import request, response
+from modules.routing.index import blueprint as index_bp
+import sys
 
-# import time
 app = Flask(__name__, template_folder=flask_app.TEMPLATE_FOLDER)
 
-# session_login = request.SessionLoginManager()
 
-# @session_login.get_user
-# def login_get_user(sql_session=None):
-#     user = User(first_name="Nikita",last_name="Balakin")
-#     sql_session.add(user)
-#     return user
+app.register_blueprint(index_bp)
 
-# @session_login.error
-# def login_error(sql_session=None,exception=None):
-#     raise exception
-
-
-@app.route("/")
-# @response.json
-# @response.user_friendly_exceptions
-# @session_login.required
-def index():
-    # print (database,user)
-    # print (database.ADMIN_USER_ROLE(user.session))
-    # print (database.ADMIN_USER_ROLE(user.session))
-    # print (database.ADMIN_USER_ROLE(user.session))
-
-    return render_template('index.html')
-
-
-@app.route("/dev/")
-def dev_index():
-    return render_template('dev/app.html')
-
+app.config['DEV'] = False
 if __name__ == '__main__':
-
+    if 'production' not in sys.argv:
+        app.config['DEV'] = True
     app.run(debug=True, host="0.0.0.0", port=8080, threaded=True)
