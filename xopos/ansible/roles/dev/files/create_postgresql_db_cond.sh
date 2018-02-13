1502b5c1b615c90a45d4b8eadd54269d4545d643
Neketek <neketek@gmail.com>
2018-02-13 03:55:32 +0200

    Comment in create db script

--- xopos/ansible/roles/dev/files/create_postgresql_db_cond.sh
+++ xopos/ansible/roles/dev/files/create_postgresql_db_cond.sh
@@ -12,9 +12,19 @@ readonly DB_CREATION_CMD="$(cat <<EOF
 psql -c "CREATE DATABASE ${DB_NAME}"
 EOF
 )"
+# readonly DB_USER_PASSWORD_SET_CMD="$(cat <<EOF
+# psql -c "ALTER USER postgres WITH PASSWORD 'postgres';
+# EOF
+# )"
+
+
 
 if ! sudo -u postgres bash -c "${DB_EXISTENCE_CHECK_CMD}"; then
     sudo -u postgres bash -c "${DB_CREATION_CMD}"
+    #NOTE: Neketek added this na kolenke solution for default password
+    #NOTE: I checked the query it's working
+    #NOTE: but it does not set the password for postgres user
+    # sudo -u postgres bash -c "${DB_USER_PASSWORD_SET_CMD}"
     exit 100
 else
     exit 200
